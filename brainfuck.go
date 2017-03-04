14095f45fbcfc9919486764d1e84b16190ca1a66
Ben Richards <blrichards@me.com>
2017-03-04 02:17:56 -0600

    refactoring and more optimizing

--- brainfuck.go
+++ brainfuck.go
@@ -6,92 +6,120 @@ import (
 	"os"
 )
 
+// UPPER uppercase letters counter index
+const UPPER = 0
+
+// LOWER lowercase letters counter index
+const LOWER = 1
+
+// PUNCT punctuation counter index
+const PUNCT = 2
+
+// OUTPUT brainfuck code to be outputed
+var OUTPUT = ""
+
+func getIdx(idx, c int) int {
+	switch {
+	case c >= 'a' && c <= 'z':
+		if idx == UPPER {
+			OUTPUT += ">>>"
+		} else if idx == PUNCT {
+			OUTPUT += "<"
+		} else {
+			OUTPUT += ">"
+		}
+		return LOWER
+	case c >= 'A' && c <= 'Z':
+		if idx == LOWER {
+			OUTPUT += "<"
+		} else if idx == PUNCT {
+			OUTPUT += "<<<"
+		} else {
+			OUTPUT += ">"
+		}
+		return UPPER
+	default:
+		if idx == UPPER {
+			OUTPUT += ">>>>>"
+		} else if idx == LOWER {
+			OUTPUT += ">>>"
+		} else {
+			OUTPUT += ">"
+		}
+		return PUNCT
+	}
+}
+
 func main() {
-	const UPPER = 0
-	const LOWER = 1
-	const PUNCT = 2
 	toConvert := os.Args[1]
 	last := [3]int{0, 0, 0}
 	idx := UPPER
-	out := ""
+
 	for _, r := range toConvert {
 		c := int(r)
 		f := 7
-		switch {
-		case c >= 'a' && c <= 'z':
-			if idx == UPPER {
-				out += ">>>"
-			} else if idx == PUNCT {
-				out += "<"
-			} else {
-				out += ">"
+
+		idx = getIdx(idx, c)
+
+		if int(math.Abs(float64(last[idx]-c))) <= 10 {
+			OUTPUT += "<"
+			for last[idx] < c {
+				OUTPUT += "+"
+				last[idx]++
 			}
-			idx = LOWER
-		case c >= 'A' && c <= 'Z':
-			if idx == LOWER {
-				out += "<"
-			} else if idx == PUNCT {
-				out += "<<<"
-			} else {
-				out += ">"
+			for last[idx] > c {
+				OUTPUT += "-"
+				last[idx]--
 			}
-			idx = UPPER
-		default:
-			if idx == UPPER {
-				out += ">>>>>"
-			} else if idx == LOWER {
-				out += ">>>"
-			} else {
-				out += ">"
-			}
-			idx = PUNCT
-		}
-		for ; f > 0; f-- {
-			r := int(math.Abs(float64(last[idx]-c))) % f
-			if r == 0 {
-				break
-			}
-			if r < 4 {
-				out += "<"
-				if last[idx] < c {
-					for ; r > 0; r-- {
-						out += "+"
-					}
-				} else {
-					for ; r > 0; r-- {
-						out += "-"
-					}
+			OUTPUT += "."
+		} else {
+			for ; f > 0; f-- {
+				r := int(math.Abs(float64(last[idx]-c))) % f
+				if r == 0 {
+					break
 				}
-				out += ">"
-				break
+				if r < 4 {
+					OUTPUT += "<"
+					if last[idx] < c {
+						for ; r > 0; r-- {
+							OUTPUT += "+"
+						}
+					} else {
+						for ; r > 0; r-- {
+							OUTPUT += "-"
+						}
+					}
+					OUTPUT += ">"
+					break
+				}
 			}
-		}
 
-		for i := f; i > 0; i-- {
-			out += "+"
-		}
-		out += "[<"
-
-		l := last[idx]
-
-		switch {
-		case l < c:
-			for i := (c - l) / f; i > 0; i-- {
-				out += "+"
+			for i := f; i > 0; i-- {
+				OUTPUT += "+"
 			}
-		case l > c:
-			for i := (l - c) / f; i > 0; i-- {
-				out += "-"
-			}
-		}
+			OUTPUT += "[<"
 
-		last[idx] = c
-		out += ">-]<."
+			l := last[idx]
+
+			switch {
+			case l < c:
+				for i := (c - l) / f; i > 0; i-- {
+					OUTPUT += "+"
+				}
+			case l > c:
+				for i := (l - c) / f; i > 0; i-- {
+					OUTPUT += "-"
+				}
+			}
+
+			last[idx] = c
+			OUTPUT += ">-]<."
+		}
 	}
 
-	for i, c := range out {
+	for i, c := range OUTPUT {
 		fmt.Printf("%c", c)
-		if i%100 == 99 {
+		if i%60 == 59 {
 			fmt.Println()
 		}
 	}
